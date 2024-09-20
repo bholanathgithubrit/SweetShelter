9212dfb7c113075807d51cdb4c7530a741e28b17
bholanathgithubrit <134517596+bholanathgithubrit@users.noreply.github.com>
2024-09-21 01:56:10 +0530

    fixing the Datepicker functionality

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,17 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      css: {
+        modules: {
+          // This allows you to import CSS files as modules
+          mode: 'icss',
+        },
+      },
+    },
+  },
+  optimizeDeps: {
+    include: ['react-datepicker/dist/react-datepicker.css'],
+  },
 })
